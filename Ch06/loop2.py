4fd361c3932501e61cc326c9778058d17872cc47
ChaehwanLee <oasisinyourmind@gmail.com>
2022-02-07 14:40:39 +0900

    time break

--- Ch06/loop2.py
+++ Ch06/loop2.py
@@ -6,6 +6,10 @@ def sendData(data):
 
 while True:
     temp = 0;
-    temp = random.randint(1,100);
+    temp = random.randint(1,10);
+    if temp == 5: # 온도가 5도면 전송하지 않는다
+        break;
     sendData(temp);
     time.sleep(3); # 3s
+
+print('Terminated ...'); # 끝나면 출력
