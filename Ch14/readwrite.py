5798c6aeffcc210732b24adf6a5df54b808f2c93
ChaehwanLee <oasisinyourmind@gmail.com>
2022-02-09 10:24:12 +0900

    2.9 commit file not found Error

--- Ch14/readwrite.py
+++ Ch14/readwrite.py
@@ -7,3 +7,14 @@ def filewrite(name, content):
     finally:
         f.close();
 
+def fileread(name):
+    try:
+        f = open(name,'rt');
+        text = f.read();
+    except:
+        raise FileNotFoundError;
+    finally:
+        f.close();
+    return text;
+
+# 기능을 분리해서 사용용
