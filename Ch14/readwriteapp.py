8f80dcf5a744f891398c04ccb0012926a01f4bc6
ChaehwanLee <oasisinyourmind@gmail.com>
2022-02-09 10:25:08 +0900

    2.9 commit User Interface

--- Ch14/readwriteapp.py
+++ Ch14/readwriteapp.py
@@ -1,11 +1,19 @@
 import readwrite;
 
 def start():
-    name = input('Input File Name...?');
-    content = input('Write Contents..:');
-    readwrite.filewrite(name,content);
+    # name = input('Input File Name...?');
+    # content = input('Write Contents..:');
+    # readwrite.filewrite(name,content);
+    rname = input('Input read File Name...?');
+    try:
+        rcontent = readwrite.fileread(rname);
+        print(rcontent);
+    except:
+        print('File Not Found...');
 
-# 마우스로 아이콘을 클릭했을 때, 휴대폰으로 앱을 눌렀을 때
+ # 마우스로 아이콘을 클릭했을 때, 휴대폰으로 앱을 눌렀을 때능
+
+# UI 를 분리해서 사용
 
 if __name__ == '__main__':
     start();
