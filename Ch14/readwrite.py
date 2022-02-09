891afbe98e9a0ae79417f73d29a3f2196939240a
ChaehwanLee <oasisinyourmind@gmail.com>
2022-02-09 11:09:43 +0900

    2.9 filename module split, UI&Function, exception

--- Ch14/readwrite.py
+++ Ch14/readwrite.py
@@ -7,16 +7,28 @@ def filewrite(name, content):
     finally:
         f.close();
 
-def fileread(name):
+# def fileread(name):
+#     try:
+#         f = open(name, 'rt');
+#         text = f.read();
+#     except FileNotFoundError as fe:
+#         raise fe;
+#     except IOError as ie:
+#         raise ie;
+#     finally:
+#         f.close();
+#     return text;
+
+def filerea(name):
+    f = None;
     try:
         f = open(name, 'rt');
         text = f.read();
     except FileNotFoundError as fe:
-        raise fe;
-    except IOError as ie:
-        raise ie;
+        raise fe; # 예외 상황에서 raise를 던진다
     finally:
-        f.close();
+        if f is not None:
+            f.close();
     return text;
 
 # 기능을 분리해서 사용
--- Ch14/readwriteapp.py
+++ Ch14/readwriteapp.py
@@ -4,15 +4,24 @@ def start():
     # name = input('Input File Name...?');
     # content = input('Write Contents..:');
     # readwrite.filewrite(name,content);
+
+    # rname = input('Input read File Name...?');
+    # try:
+    #     rcontent = readwrite.fileread(rname);
+    #     print(rcontent);
+    # except:
+    #     print('File Not Found...');
+
     rname = input('Input read File Name...?');
     try:
         rcontent = readwrite.fileread(rname);
         print(rcontent);
-    except:
+    except FileNotFoundError:
         print('File Not Found...');
+    except IOError:
+        print('IO Error');
 
- # 마우스로 아이콘을 클릭했을 때, 휴대폰으로 앱을 눌렀을 때능
-
+# 마우스로 아이콘을 클릭했을 때, 휴대폰으로 앱을 눌렀을 때
 # UI 를 분리해서 사용
 
 if __name__ == '__main__':
